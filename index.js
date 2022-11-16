27685f37e3b7afd86e67f2ddc7d19d8646086321
Gleb Andreev <gleb.erend@gmail.com>
2022-11-16 15:20:47 +0300

    Some fixes

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function write() {
 function read() {
     // read the 2 registers starting at address 5
     // on device number 1.
-    console.log('try to read, id:', client.getId());
+    console.log('try to read, id:', client.getID());
     // client.setID(2);
     // console.log('now id:', client.getID());
     // client.readHoldingRegisters(0, 5)
     //     .then(console.log);
-    client.writeFC3 (2, 0x1000, 8, console.log);
+    // client.writeFC3(2, 0x1000, 8, console.log);
     for (let index = 0; index < 10; index++) {
         try {
             client.writeFC3 (index, 0x1000, 8, console.log);
