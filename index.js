dd8e8c43c62642176afe14c05d1f5bb0ef8f849e
Gleb Andreev <gleb.erend@gmail.com>
2022-11-17 19:27:42 +0300

    Add await

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function read() {
       
         for (let index = 0; 100; index++) {
         //   const data = await client.writeFC3 (2, 0, 16, (err, data))
-        const data = client.readHoldingRegisters (0, 8);
+        const data = await client.readHoldingRegisters (0, 8);
           console.log('data', data);
         }
       
