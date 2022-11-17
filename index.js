064158044ffb1fa6c88440f16e0bac872200b95d
Gleb Andreev <gleb.erend@gmail.com>
2022-11-17 19:43:17 +0300

    Direction

--- index.js
+++ index.js
@@ -20,27 +20,33 @@ function read() {
     // set a timout for requests default is null (no timeout)
     client.setTimeout(500)
 
-    // read the 2 registers starting at address 5
-    // on device number 1.
-    // console.log('id:', client.getID());
-    // client.setID(2);
-    // console.log('now id:', client.getID());
-    // client.readHoldingRegisters(0, 5)
-    //     .then(console.log);
-    // client.writeFC3(2, 0x1000, 8, console.log);
+    // Speed
+    // const forLoop = async () => {
+    //     console.log('Start')
+      
+    //     for (let index = 0; 100; index++) {
+    //     //   const data = await client.writeFC3 (2, 0, 16, (err, data))
+    //     const data = await client.readHoldingRegisters (0, 1);
+    //       console.log('Wind speed: ', data.data[0] / 10);
+    //       console.log('Buffer: ', data.buffer);
+    //     }
+      
+    //     console.log('End')
+    //   }
 
+    // Direction
     const forLoop = async () => {
         console.log('Start')
-      
+        
         for (let index = 0; 100; index++) {
         //   const data = await client.writeFC3 (2, 0, 16, (err, data))
         const data = await client.readHoldingRegisters (0, 1);
-          console.log('Wind speed: ', data.data[0] / 10);
-          console.log('Buffer: ', data.buffer);
+        console.log('Direction: ', data.data[0]);
+        console.log('Buffer: ', data.buffer);
         }
-      
+        
         console.log('End')
-      }
+    }
 
     forLoop();
 
